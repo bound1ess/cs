aa48de8ed2c56bf33201cf4274016344d5abc60b
bound1ess <bound1ess.dev@gmail.com>
2015-03-26 22:32:32 +0600

    Implement Selection sorting algorithm.

---
+++ src/algs/sorting/selection.rb
@@ -0,0 +1,23 @@
+module Algorithms
+    module Sorting
+        class Selection
+            def sort(array)
+                for index in 0..(array.size - 2)
+                    min_index = array.find_index(array.slice(index, array.size - index).min)
+
+                    array = swap(array, index, min_index)
+                end
+
+                array
+            end
+
+            def swap(array, index1, index2)
+                tmp = array[index1]
+                array[index1] = array[index2]
+                array[index2] = tmp
+
+                array
+            end
+        end
+    end
+end
