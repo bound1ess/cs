7e69547ff1b4e26d72aa8658af438c742b67af22
bound1ess <bound1ess.dev@gmail.com>
2015-03-27 18:34:28 +0600

    Finish Merge sort.

--- src/algs/sorting/merge.rb
+++ src/algs/sorting/merge.rb
@@ -2,6 +2,7 @@ module Algorithms
     module Sorting
         class Merge
             def sort(array)
+                # Otherwise it's sorted (0 elements or only 1 element).
                 return array if array.size < 2
 
                 middle = array.size / 2
@@ -13,19 +14,28 @@ module Algorithms
             def merge(left, right)
                 merged = []
 
+                puts "Working with subsets [#{left.join(', ')}] and [#{right.join(', ')}]..."
+
+                # Keep merging until we run out of elements in either left or right subset(s).
                 while left.size > 0 or right.size > 0
+                    # If both subsets are not empty, we'll need to compare two values.
                     if left.size > 0 and right.size > 0
+                        # Merge the correct value depending on outcome of the comparison.
                         if left.first <= right.first
                             merged.push(left.shift)
                         else
                             merged.push(right.shift)
                         end
+                    # As described below, but for the left subset.
                     elsif left.size > 0
                         merged.push(left.shift)
+                    # If the right subset is not empty, merge and delete its first element.
                     elsif right.size > 0
                         merged.push(right.shift)
                     end
                 end
+
+                puts "After merging: [#{merged.join(', ')}]"
 
                 merged
             end
