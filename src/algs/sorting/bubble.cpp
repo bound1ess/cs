40a810e67effd50b0979856d4fc9ff235e6d1b31
bound1ess <bound1ess.dev@gmail.com>
2015-08-11 17:01:38 +0600

    Add awesome bubble sort.

---
+++ src/algs/sorting/bubble.cpp
@@ -0,0 +1,41 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+inline void bubble_sort(int arr[], int l, int r);
+
+int main() {
+  int n; scanf("%d", &n);
+  int* arr = new int[n];
+
+  for (int i = 0; i < n; ++i) {
+    scanf("%d", arr + i);
+  }
+
+  bubble_sort(arr, 0, n - 1);
+
+  for (int i = 0; i < n; ++i) {
+    printf("%d", arr[i]);
+    printf(i + 1 < n ? " " : "\n");
+  }
+
+  return 0;
+}
+
+inline void bubble_sort(int arr[], int l, int r) {
+  bool keep = true;
+
+  while (keep) {
+    keep = false;
+
+    for (int i = l; i < r; ++i) {
+      if (arr[i] > arr[i + 1]) {
+        swap(arr[i], arr[i + 1]);
+        keep = true;
+      }
+    }
+
+    --r;
+  }
+}
