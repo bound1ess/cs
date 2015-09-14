c54953ed3764de01ffbdb8422917669ab96ab882
bound1ess <bound1ess.dev@gmail.com>
2015-09-14 23:23:52 +0600

    Update cycle detection algorithm for undirected graphs.

--- src/algs/graphs/cycles/undirected.cpp
+++ src/algs/graphs/cycles/undirected.cpp
@@ -4,28 +4,30 @@
 using namespace std;
 
 vector< vector<int> > adj;
-vector<bool> visited;
+vector<bool> visited, rec_stack;
 
 bool check(int v, int parent = -1) {
-  visited[v] = true;
+  if ( ! visited[v]) {
+    rec_stack[v] = visited[v] = true;
 
-  for (int i: adj[v]) {
-    if ( ! visited[i]) {
-      if (check(i, v)) {
+    for (int i: adj[v]) {
+      if ( ! visited[i] && check(i, v)) {
         return true;
       }
-    } else if (i != parent) {
-      return true;
+
+      if (rec_stack[i] && i != parent) {
+        return true;
+      }
     }
   }
 
-  return false;
+  return rec_stack[v] = false;
 }
 
 int main() {
   int n, m;
   scanf("%d%d", &n, &m);
-  adj.resize(n), visited.resize(n);
+  adj.resize(n), visited.resize(n), rec_stack.resize(n);
 
   for (int i = 0; i < m; ++i) {
     int u, v;
@@ -35,7 +37,7 @@ int main() {
   }
 
   for (int i = 0; i < n; ++i) {
-    if ( ! visited[i] && check(i)) {
+    if (check(i)) {
       printf("There is a cycle\n");
       return 0;
     }
